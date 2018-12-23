4529c8c1f05fedb5d2626eee246553107236f9a0
user name <email@email.com>
2018-12-23 14:02:15 +0000

    Add runCleanUp removes previous scan files

--- tools/live_hosts.sh
+++ tools/live_hosts.sh
@@ -3,14 +3,41 @@
 network=network.txt
 hosts=live_hosts.txt
 green='\033[0;32m'
+red='\033[0;31m'
 esc='\033[0m'
+text='blank message'
 
+greenMsg(){
+  printf "${green} ${text} ${esc}\n"
+}
+
+redMsg(){
+  printf "${red} ${text} ${esc}\n"
+}
+
+
+runCleanUp(){
+  [[ -f $hosts ]] && echo "Running housekeeping.."
+  if [ -f $hosts ]; then
+    text="[-] Remove file $hosts $esc"
+    redMsg
+    rm $hosts
+  fi
+  if [ -f $network ]; then
+    text="[-] Remove file $network $esc"
+    redMsg
+    rm $network
+  fi
+}
+
+runCleanUp
 printf "Scanning network $1 please standy\n"
 nmap $1 -n -vvv -sn | grep report | awk '{print $5}' > $network
 
-printf "${green}[+] Network scan completed - [ $network ] created ${esc}\n"
-
-printf "${green}[+] File created - [ $hosts ]  created ${esc}\n"
+text="[+] Network scan completed - [ $network ] file created"
+greenMsg
+text="[+] Discovered live hosts - [ $hosts ] file created"
+greenMsg
 printf "Found live hosts:\n\n"
 
 arp -an | sed '/\(incomplete\)/d' | tee $hosts
