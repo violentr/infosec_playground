ac760d1e6a0d054f3a7d669af4f8c644b5479a7b
user name <email@email.com>
2018-12-23 12:34:14 +0000

    Add Output discovered live hosts to txt file

--- tools/live-hosts.sh
+++ tools/live-hosts.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-nmap $1 -n -sP | grep report | awk '{print $5}'
+outFile1=network.txt
+outFile2=live_hosts.txt
+
+printf "Scanning network $1 please standy\n"
+
+nmap $1 -n -vvv -sn | grep report | awk '{print $5}' > $outFile1
+
+printf "[+] Network scan completed - [ $outFile1 ] was created\n"
+
+printf "Found live hosts:\n"
+
+printf "[+] File - [ $outFile2 ]  was created\n"
+
+arp -an | sed '/\(incomplete\)/d' | tee $outFile2
+#arp -a -n | awk '{print $2 $4}' | sed '/\(incomplete\)/d'
