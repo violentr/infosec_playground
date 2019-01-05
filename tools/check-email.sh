5f6171e6901a0c566d9b2e3fec377a7db74bc9ac
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-01-05 18:30:18 +0000

    Add bash tool checks if emails legit

---
+++ tools/check-email.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+filename=$1
+output_file="./legit-emails.txt"
+
+handle_file(){
+  if [ -e "$output_file" ]; then
+    `rm $output_file`
+    echo "*****************************************************"
+    echo " Previous version of '$output_file' was removed "
+    echo "*****************************************************"
+  fi
+}
+
+IFS=$'\n'
+handle_file
+
+for email_domain in `cat $filename`
+  do
+    domain=`echo $email_domain |cut -d"@" -f2`
+    check_result=`dig mx $domain |grep MX |wc -l`
+    if (( $check_result > 1 )); then
+      #this is probably legit email
+      echo "$email_domain" | tee -a $output_file
+    fi
+  done
+
+echo -e " \t *************"
+echo -e " \t  Completed ! "
+echo -e " \t *************"
+
+exit 0
