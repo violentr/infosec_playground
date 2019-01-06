c28fb63e957b4a5a6e097d8f5fe8e2285c327405
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-01-06 17:01:12 +0000

    Add GeoLocation with GoogleMaps, opens in browser

--- tools/dig-domain.sh
+++ tools/dig-domain.sh
@@ -2,6 +2,8 @@
 
 domain=$1
 dnsName=google
+googleMaps=https://www.google.com/maps
+hackerTargetApi=https://api.hackertarget.com/geoip/?q
 
 selectDns(){
     case $dnsName in
@@ -11,12 +13,19 @@ selectDns(){
     esac
 }
 
-
 domainTransfer(){
   echo -e "\n"
   echo "[+] Nameserver is set to $dnsName" |tr a-z A-Z
   echo -e "Trying domain transfer.."
   dig "@$dns" $domain axfr
+}
+
+getGeoLocation(){
+  printf "\n"
+  echo -e "GEO ip location: \n"
+  curl $hackerTargetApi=$domain |tee output.txt
+  array=($(cat output.txt  | tail -2 | awk {'print $2'} | cut -d':' -f1))
+  gps="$googleMaps/@${array[0]},${array[1]},10z"
 }
 
 echo -e "\n"
@@ -28,4 +37,7 @@ dig $domain AAAA +short
 
 selectDns
 domainTransfer
-
+echo "Opening Google maps with defined coordinates"
+getGeoLocation
+open /Applications/Google\ Chrome.app $gps
+printf "\n"
