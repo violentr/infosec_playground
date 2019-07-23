06956d9e1be757ae948129eab8abe5f9e8294ee1
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-07-23 18:32:57 +0100

    Add better way how to handle socket connection
    created socket after use will be closed
    automatically this handled by "with"
    statement
    https://effbot.org/zone/python-with-statement.htm

--- reconasance/vulnscan.py
+++ reconasance/vulnscan.py
@@ -4,21 +4,27 @@ import os, sys
 import socket
 from termcolor import colored
 
-ip = "172.16.122.131"
+ip = input("Enter host to scan: ") or "172.16.122.131"
 ports = [21,22,23,25,80,443,110]
+default_timeout = 5
 
 def get_banner(ip, port):
     try:
-        socket.setdefaulttimeout(2)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, port))
-        return sock.recv(1024)
+        socket.setdefaulttimeout(default_timeout)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            result = s.connect_ex((ip, port))
+            print(result)
+            if result == 0:
+                return s.recv(1024)
+            else:
+                print(colored("[-] IP %s/%d has no results" % (ip, port), "red"))
 
     except socket.timeout:
-        print(colored("Time is up", "red"))
+        print(colored("[-] TimeOut for %s/%d" % (ip, port), "red"))
+
     except ConnectionRefusedError:
+        print("Connection was refused")
         pass
-        #print("Connection was refused")
     except KeyboardInterrupt:
         print(colored("Crl + C was pressed", 'red'))
         exit(0)
@@ -35,17 +41,20 @@ def checkVuln(banner, filename):
 
 def main():
     global ip, ports
-
     if len(sys.argv) == 2:
         filename = sys.argv[1]
+
         if not os.path.isfile(filename):
             print(colored("[-] File %s does not exist" % filename, 'red'))
+            exit(0)
+
         if not os.access(filename, os.R_OK):
             print(colored("[-] Access is denied!", 'red'))
             exit(0)
     else:
         print("Usage: %s <vulns filename>" % sys.argv[0])
         exit(0)
+
     for port in ports:
         banner = get_banner(ip, port)
         if banner:
