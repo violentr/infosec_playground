99a1a960f131dd22f56ff9a36a922ed9b0f7b318
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-01-11 10:51:15 +0000

    Add Server static methods :extract_filename, :check_input

--- tools/Python/c2c/http_server.py
+++ tools/Python/c2c/http_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import http.server as http_server
 import re
+
 #Python 3
 #HTTP server
 
@@ -11,7 +12,7 @@ CONFIG = (HOSTNAME, PORT_NUMBER)
 class MyHandler(http_server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        command = check_input()
+        command = self.check_input()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -25,23 +26,25 @@ class MyHandler(http_server.BaseHTTPRequestHandler):
         response = _post.decode()
         print(response)
         if "filename=" in response:
-            filename = extract_filename(response)
+            filename = self.extract_filename(response)
             with open(filename, 'w') as f:
                 f.write(response)
 
-def extract_filename(text):
-    headers = re.split('\r\n', text)
-    match = re.search("(filename=\".*\w+\.\w.*$)" , headers[1])
-    strings  = match[0].split('"')
-    attr, filename = [x for x in strings if x]
-    return filename
+    @staticmethod
+    def extract_filename(text):
+        headers = re.split('\r\n', text)
+        match = re.search("(filename=\".*\w+\.\w.*$)" , headers[1])
+        strings  = match[0].split('"')
+        attr, filename = [x for x in strings if x]
+        return filename
 
-def check_input():
-    command = input("Shell> ")
-    if len(command) > 0:
-        return command
-    else:
-        return 'ls -lA'
+    @staticmethod
+    def check_input():
+        command = input("Shell> ")
+        if len(command) > 0:
+            return command
+        else:
+            return 'ls -lA'
 
 if __name__ == '__main__':
     try:
