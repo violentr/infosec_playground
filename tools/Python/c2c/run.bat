77bba6fd2ac3c57ee9ab77bb72ea670c99fb2d63
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-01-12 13:03:09 +0000

    Add Bat file, make executable from python code

---
+++ tools/Python/c2c/run.bat
@@ -0,0 +1,6 @@
+#This will compile python source code into executable
+
+cd c:\Users\user\AppData\Local\Programs\Python\Python38\Scripts
+
+pyinstaller.exe -F -w c:\Users\user\Desktop\c2c\client.py
+
