25f8b922a0f7c3cf5439e7de707c541e0a3d0e46
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-05-24 09:17:35 +0100

    Add Improve script , by performing necessary setup check

--- tools/connect_sk5.sh
+++ tools/connect_sk5.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
-
-proxyPort=8081
+proxyPort=
 remoteHost=
 sshKey=
 
-echo -e "[+] Closing previously opened proxy port: $proxyPort"
-lsof -i TCP:$proxyPort | awk '/LISTEN/ {print $2}' | xargs kill -9
+function doCheck(){
+  [ -z $proxyPort ] && echo -e "[-] No socks5 port defined" && exit 1
+  [ -z $remoteHost ] && echo -e "[-] No remote host defined" && exit 1
+  [ -z $sshKey ] && echo -e "[-] No SSH key defined" && exit 1
+}
+
+doCheck
+curPid=$(lsof -i TCP:$proxyPort | awk '/LISTEN/ {print $2}')
+
+if [ ! -z "$curPid" ];then
+  echo -e "[+] Closing previously opened proxy port: $proxyPort"
+  kill -9 $curPid
+fi
 
 echo -e "Connecting to remote host: $remoteHost"
 ssh -D $proxyPort -i $sshKey -N -f ubuntu@$remoteHost
 
 echo -e "[+] Proxing local traffic through socks5, port: $proxyPort\n"
-export http_proxy=socks5://127.0.0.1:$proxyPort
 
-netstat -antp |grep LISTEN |grep $proxyPort
+netstat -ant |grep LISTEN |grep $proxyPort
+echo -e "\n[+] Run this command now: export http_proxy=socks5://127.0.0.1:$proxyPort"
