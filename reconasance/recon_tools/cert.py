606966a5a7bc6733e08565aa4b5a00f99a146b55
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2024-06-26 13:56:49 +0100

    Add 2 modes, brute and individual scan for certificate

--- reconasance/recon_tools/cert.py
+++ reconasance/recon_tools/cert.py
@@ -11,18 +11,18 @@ hostname = sys.argv[1]
 filename = f"{hostname}-output.txt"
 
 def format(string):
-    domain = string.split("=")[1]
+    domain = string.split("CN=")[1]
     domain = re.sub("\)", "", domain)
     return re.sub(">", "", domain)
 
 
-def run_scan(ip_string):
+def grep_certificate_subject(ip_string):
     global port
     cert = ssl.get_server_certificate((ip_string, port))
     certDecoded = x509.load_pem_x509_certificate(str.encode(cert), default_backend)
     cert = format(str(certDecoded.issuer))
     subject = format(str(certDecoded.subject))
-    msg = f'{hostname} | {subject} \n'
+    msg = f'{ip_string} | {subject} \n'
     if filtered:
         match = re.search(search_criteria[0], subject)
         if match:
@@ -30,20 +30,23 @@ def run_scan(ip_string):
     else:
         return msg
 
-def process():
-    global hostname
+def run_individual_scan(hostname):
+    try:
+        result = grep_certificate_subject(hostname)
+        print(result)
+        write_tofile(result)
+    except KeyboardInterrupt as e:
+        print("[!] Ctrl+C detected, exiting now ..")
+        sys.exit(1)
+    except Exception as e:
+        print(f'[!] Timeout can\'t connect to {hostname}')
+
+def run__range_scan(hostname):
     for i in range(1, 255):
-        try:
-            hostname = ".".join(hostname.split(".")[:3])
-            hostname = f'{hostname}.{i}'
-            result = run_scan(hostname)
-            print(result)
-            write_tofile(result)
-        except KeyboardInterrupt as e:
-            print("[!] Ctrl+C detected, exiting now ..")
-            sys.exit(1)
-        except Exception as e:
-            print(f'[!] Timeout can\'t connect to {hostname}')
+        hostname = ".".join(hostname.split(".")[:3])
+        hostname = f'{hostname}.{i}'
+        print("Hostname", hostname)
+        run_individual_scan(hostname)
     
 def write_tofile(data):
     global filename
@@ -53,11 +56,13 @@ def write_tofile(data):
     except Exception as e:
         print(e)
 
+def process():
+    global hostname
+    #run_individual_scan()
+    run__range_scan(hostname)
 
 def main():
-    data = process()
-    write_tofile(data)
-
+    process()
 
 if __name__ == "__main__":
     main()
